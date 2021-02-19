ff1f8512234ee87603d7362d91c0fdc37bcba4c1
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-02-19 17:34:42 +0100

    Sidebar is now responsive and toggling on clicking button i app component. Sidebar is now hiding partly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
