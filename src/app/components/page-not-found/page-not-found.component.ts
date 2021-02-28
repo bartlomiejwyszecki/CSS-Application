cb08ae5b476b433428de4af92f2905af77f8afc7
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-02-28 19:17:01 +0100

    Page-not-found completed.

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -3,11 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found works!
-    </p>
+    <div class="flexbox">
+      <div class="header">
+        <h1><i class="bi bi-search"></i></h1>
+      </div>
+      <div class="content">
+        <span>Oops, looks like the page you are looking for does not exist.</span>
+        <span>Check if your website adress is correct.</span>
+      </div>
+    </div>
   `,
   styles: [
+    '.flexbox { display: flex; flex-wrap: wrap; align-items: center; margin-top: 100px; }',
+    '.header { width: 100% }',
+    'h1 { text-align: center; }',
+    '.content { width: 100%; text-align: center; margin-top: 50px; }',
+    'span { font-weight: 600; width: 100%; display: inline-block; line-height: 1.6; font-size: 1.3em; }'
   ]
 })
 export class PageNotFoundComponent implements OnInit {
